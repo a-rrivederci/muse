2f9af21f76afdb4a0a1de2ef3130c898febc45a4
yasster <ymissoum1@gmail.com>
2017-11-30 19:51:08 -0800

    Rewriting the Server

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var port = process.env.PORT || 8080;
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/file_load', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        var ext = require('path').extname(file.originalname);
+        ext = ext.length>1 ? ext : "." + require('mime').extension(file.mimetype);
+        require('crypto').pseudoRandomBytes(16, function (err, raw) {
+            cb(null, (err ? undefined : raw.toString('hex') ) + ext);
+        });
+    }
+});
+
+var upload = multer({ storage: storage });
+
+app.post('/file_upload', upload.any(), function (req, res, next) {
+    res.send(req.files);
+})
+app.listen(port);
+console.log('The server is running on: ' + port);
