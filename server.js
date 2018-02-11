20f15d78add83caf018f343fd30bc8ff3e24041d
Elvis Eshikena <elviseshikena@gmail.com>
2018-02-11 04:27:15 -0800

    Added audio and images cleanup functionality

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var fs = require("fs");
 var port = process.env.PORT || 8080;
 var multer  = require('multer');
 var path = require('path');
+var p_ = require('path');
 var functions = require('./js/functions')
 
 //Variables
@@ -34,6 +35,7 @@ app.use(express.static("views"));
 app.post('/file_upload', upload.single('Image'), function (req,res,err) {    
     console.log(req.file);
     var imgLocation = req.file.path
+    console.log(imgLocation);
     var imagePath = __dirname + '/' + imgLocation;
     //res.sendFile(__dirname + '/'+ imgLocation);
     app.get('/image',function(req,res) {
@@ -41,12 +43,36 @@ app.post('/file_upload', upload.single('Image'), function (req,res,err) {
         res.writeHead(200,{'Content-Type': 'image/png'});
         res.end(img,'png');
     })
-    res.sendFile(path+ 'index.html');
+    res.sendFile(path + 'index.html');
     functions.RunTesseract(req);
 });
 
 app.get('/retrive', function(req,res){
-    res.download('./audio/Text.mp3');
+    // download mp3
+    var audioLocation = './audio/Text.mp3'
+    res.download(audioLocation);
+
+    // cleanup images
+    fs.readdir('uploads', (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(p_.join('uploads', file), err => {
+                if (err) throw err;
+            });
+        }
+    });
+
+    // clean mp3 file
+    fs.readdir('audio', (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(p_.join('audio', file), err => {
+                if (err) throw err;
+            });
+        }
+    });
 })
 
 app.listen(port);
